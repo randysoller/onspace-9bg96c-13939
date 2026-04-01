95df14b373eed7c3d441d5c7d89b42e2c291b8d8
onspace.ai <onspace.ai@gmail.com>
2026-04-01 13:31:36 -0500

    Increase diamond finger number size to match orange dots on mobile

--- src/components/features/ChordDiagram.tsx
+++ src/components/features/ChordDiagram.tsx
@@ -274,7 +274,7 @@ function ChordDiagramBase({ chord, size = 'md', className = '' }: ChordDiagramPr
                         textAnchor="middle"
                         dominantBaseline="middle"
                         className="fill-white font-black"
-                        style={{ fontSize }}
+                        style={{ fontSize: fontSize * 1.2 }}
                       >
                         {finger}
                       </text>
@@ -337,7 +337,7 @@ function ChordDiagramBase({ chord, size = 'md', className = '' }: ChordDiagramPr
                   textAnchor="middle"
                   dominantBaseline="middle"
                   className="fill-white font-black"
-                  style={{ fontSize }}
+                  style={{ fontSize: fontSize * 1.2 }}
                 >
                   {finger}
                 </text>
